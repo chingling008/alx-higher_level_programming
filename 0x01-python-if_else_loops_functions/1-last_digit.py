2214ffae51b05b422fbb772ff61243a337e3c278
chingling008 <chinglingmadness@gmail.com>
2023-12-01 13:25:52 -0800

    why Rise when you can break the ladder

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,11 @@ strnumber = str(number)
 lastDigit = strnumber[len(strnumber)-1]
 intLast = int(lastDigit)
 if intLast == 0:
-    print("Last Digit of " + strnumber + " is " + lastDigit + " and is 0")
+    print("Last digit of " + strnumber + " is " + 
+            lastDigit + " and is 0")
 elif number < 1:
-    print("Last Digit of " + strnumber + " is -" + lastDigit + " and is less than 6 and not 0")
+    print("Last digit of " + strnumber + " is -" + lastDigit + " and is less than 6 and not 0")
 elif intLast > 5:
-    print("Last Digit of " + strnumber + " is " + lastDigit + " and is greater than 5")
+    print("Last digit of " + strnumber + " is " + lastDigit + " and is greater than 5")
 elif intLast < 6:
-    print("Last Digit of " + strnumber + " is " + lastDigit + " and is less than 6 and not 0")
+    print("Last digit of " + strnumber + " is " + lastDigit + " and is less than 6 and not 0")
