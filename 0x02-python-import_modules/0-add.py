9b8a1359be77acb7813c9f8816d61e1c101641b5
chingling008 <chinglingmadness@gmail.com>
2023-12-03 02:48:40 -0800

    The File of life

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
 
-    Args:
-        a: first integer
-        b: second integer
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
