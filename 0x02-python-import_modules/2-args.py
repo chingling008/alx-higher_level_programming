353610c0251f591784b452106729bec9878e8e2d
chingling008 <chinglingmadness@gmail.com>
2023-12-03 03:50:06 -0800

    Ye is

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,13 @@ if __name__ == '__main__':
     n = len(sys.argv)
 
     if n == 1:
-        print(str(n - 1) + " argument.")
-
+        print(str(n - 1) + " arguments.")
     else:
-        print(str(n - 1) + " arguments:")
-        for i in range(1, n):
-            p = str(i)
-            print(p + ": ", sys.argv[i])
-
+        if n == 2:
+            print(str(n - 1) + " argument:")
+            print("1: ", sys.argv[1])
+        else:
+            print(str(n - 1) + " arguments:")
+            for i in range(1, n):
+                p = str(i)
+                print(p + ": ", sys.argv[i])
