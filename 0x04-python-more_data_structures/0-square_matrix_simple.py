96dd153f79ab69fc770ca46587b8df470c4556a0
chingling008 <chinglingmadness@gmail.com>
2023-12-06 02:11:49 -0800

    L

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+def square_matrix_simple(matrix=None):
+    if matrix is None:
+        matrix = []
+
+    firstL = len(matrix)
+    finalA = []
+
+    for i in range(firstL):
+        secondL = len(matrix[i])
+        miniA = []
+        finalA.append(miniA)
+
+        for j in range(secondL):
+            miniA.append(matrix[i][j] ** 2)
+
+    return finalA
