90dfea911dcb318149025f4921df3897aba56a22
chingling008 <chinglingmadness@gmail.com>
2023-12-06 03:21:19 -0800

    yes

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    unique_elements_set = set()
+
+    for element in set_1:
+        if element not in set_2:
+            unique_elements_set.add(element)
+
+    for element in set_2:
+        if element not in set_1:
+            unique_elements_set.add(element)
+
+    return unique_elements_set
