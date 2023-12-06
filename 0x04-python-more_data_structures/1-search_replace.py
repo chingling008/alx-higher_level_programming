9b8181a23c5eeb7a05e20e26f3f36e0ee3ba70d1
chingling008 <chinglingmadness@gmail.com>
2023-12-06 02:31:21 -0800

    L

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    arrL = len(my_list)
+    new_list = my_list.copy()
+
+    i = 0
+    while i < arrL:
+        if new_list[i] == search:
+            new_list[i] = replace
+        i += 1
+    return new_list
