b5b137d1e73a8374420e6438430acea98acad6ad
chingling008 <chinglingmadness@gmail.com>
2023-12-06 02:48:44 -0800

    yes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    result = 0
+
+    for i in range(len(my_list)):
+        if all(my_list[i] != my_list[j] for j in range(i)):
+            result += my_list[i]
+            unique_numbers.add(my_list[i])
+
+    return result
