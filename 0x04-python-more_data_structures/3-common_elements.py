4a764e57e36d7501f4b22a8f6011b45f4a1a0d9a
chingling008 <chinglingmadness@gmail.com>
2023-12-06 03:09:28 -0800

    yes

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common_set = set()
+
+    iterator_1 = iter(set_1)
+    while True:
+        try:
+            element = next(iterator_1)
+            if element in set_2:
+                common_set.add(element)
+        except StopIteration:
+            break
+
+    return common_set
