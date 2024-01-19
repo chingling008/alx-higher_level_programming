580dac2ee67b0cb21b4b11a70c2e4d3255d5ebf1
chingling008 <chinglingmadness@gmail.com>
2024-01-19 05:20:50 -0800

    Errors are There

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,11 +3,12 @@
 
 
 def class_to_json(obj):
-    """Serialize class attributes to dictionary
-    Args:
-        obj (object): object to be serialized
     """
-    return (obj.__dict__)
+    returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization of
+    an object
+    """
+    return obj.__dict__
 
 
 class Student:
@@ -24,18 +25,28 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Serializes object to dictionary"""
-        return class_to_json(self)
+    def to_json(self, attrs=None):
+        """Serializes object to dictionary
+        Args:
+            attr (list): list of attributes to filter object dictionary
+        Returns:
+            object dictionary
+        """
+        obj_dict = class_to_json(self)
+        if not attrs:
+            return obj_dict
+        else:
+            filter_dict = {}
+            for k, v in obj_dict.items():
+                if k in attrs:
+                    filter_dict[k] = v
+            return filter_dict
 
-
-if __name__ == '__main__':
-    students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-    for student in students:
-        j_student = student.to_json()
-        print(type(j_student))
-        print(j_student['first_name'])
-        print(type(j_student['first_name']))
-        print(j_student['age'])
-        print(type(j_student['age']))
+    def reload_from_json(self, json):
+        """Updates attributes through JSON file
+        Args:
+            json (dict): dictionary of JSON file
+        """
+        for k, v in json.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
